842ceaf1ad0121c84e078b91e4e122e4f9c40b4e
epociask <empociask@dons.usfca.edu>
2022-06-03 03:23:15 -0700

    [PUSH_WORKS] Got all push instructions working with tests passing

--- vm/memory.py
+++ vm/memory.py
@@ -1,17 +1,27 @@
+import sys
+
 """
-  * The memory model is a simple word-addressed byte array. 
+  YELLOW PAPER:
+  * The memory model is a simple word-addressed byte array.
+
+  Implementation inspiration taken directly from geth:
+  - https://github.com/ethereum/go-ethereum/blob/b1e72f7ea998ad662166bcf23705ca59cf81e925/core/vm/memory.go
 """
+
 
 class Memory():
   store: list = []
   limit: int = 1000000
 
+  def __init__(self):
+    self.store = [None] * self.limit
+
   def set(self, offset: int, value: bytes):
-    size = len(value)
+    size = sys.getsizeof(value)
 
     if size > 0:
-      if offset + size > self.limit:
+      if offset + size < self.limit:
         self.store[offset] = value
 
   def get(self, offset: int):
-    return self.storage[offset]
+    return self.store[offset]
